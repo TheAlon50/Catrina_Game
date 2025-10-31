89435ad69db6f16015437d3d8d0da753ccbd009f
Mario-trapito <laborinlopezkarlamariel.cb11@gmail.com>
2025-10-30 23:14:43 -0700

    Squashed commit of the following:
    commit 5138bbb5b9fcabe612e5888c9f0aba2ee3f8d497
    Author: Alonso <jesusarr590@gmail.com>
    Date:   Thu Oct 30 23:06:22 2025 -0700
    
        GameOver/ColisionAtaque/3vidas
    
    commit 481ba1af35d7b72a60cee736e59698769890beb0
    Merge: d5f9c93 d19e167
    Author: Alonso <jesusarr590@gmail.com>
    Date:   Thu Oct 30 19:24:55 2025 -0700
    
        Merge remote-tracking branch 'origin/Mario_Branch' into alon
    
    commit d5f9c9350d9f230301641bc383e368e1a80239b9
    Author: Alonso <jesusarr590@gmail.com>
    Date:   Sat Oct 25 22:39:49 2025 -0700
    
        MariaJose
    
        tiene animaciones y controles, le falta colisión de ataque y recibir daño

--- catrina-games/Scrips/Player/Movoimiento_(player).gd
+++ catrina-games/Scrips/Player/Movoimiento_(player).gd
@@ -5,21 +5,27 @@ const SPEED = 130.0
 const JUMP_VELOCITY = -340.0
 var esta_atacando: bool = false
 var is_dead: bool = false # Bloquea el control durante el respawn
+var live := 3
 
 @onready var anima := $AnimationPlayer
 @onready var Sprint := $Sprite2D
-
+@onready var colicion_ataque:= $Area2D
 func _ready() -> void:
 	anima.animation_finished.connect(_on_animation_finished)
-
+	$Area2D/CollisionShape2D.disabled = true
 
 func _physics_process(delta: float) -> void:
 	if is_dead:
 		velocity = Vector2.ZERO
 		return
-
+	
+	if direccion < 0:
+		colicion_ataque.position.x = -33
+	elif direccion != 0:
+		colicion_ataque.position.x = 1
+	
 	Sprint.flip_h = direccion < 0 if direccion != 0 else Sprint.flip_h
-
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
@@ -34,7 +40,7 @@ func _physics_process(delta: float) -> void:
 	elif Input.is_action_just_pressed("atacar"):
 		esta_atacando = true
 		anima.play("golpear")
-		
+		$Area2D/CollisionShape2D.disabled = false
 	else:
 		if not is_on_floor():
 			if velocity.y > 0:
@@ -65,17 +71,34 @@ func _on_animation_finished(anim_name: StringName):
 
 	if anim_name == "golpear":
 		esta_atacando = false
+		$Area2D/CollisionShape2D.disabled = true
 	elif anim_name == "resopanw":
 		is_dead = false
 		
 
-
+func GameOver():
+	is_dead = false
+	get_tree().change_scene_to_file("res://Escenas/GameOver/GAMEOVER.tscn")
+	
 func respawn():
 	is_dead = true
-	global_position = Vector2(27, 214)
-	anima.play("resopanw")
+	live -=1
+	if live == 0:
+		GameOver()
+	else:
+		global_position = Vector2(27, 214)
+		anima.play("resopanw")
+		
+		#Liberación automática de seguridad por si la animación no emite signal
+		await get_tree().create_timer(0.95).timeout 
+		if is_dead:
+			is_dead = false
 
-	#Liberación automática de seguridad por si la animación no emite signal
-	await get_tree().create_timer(0.95).timeout 
-	if is_dead:
-		is_dead = false
+
+	#detector de colicion en ataque
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	pass #para que no crache el juego/ quitalo despues
+	
+	#para eliminar los enemigos de un toque / mejor agregar llamada de golpe a tu enemigoo
+	#if body.is_in_group("enemigos")
+		#body.queue_free()
