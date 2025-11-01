f53a803e0891e23a52e6b00127253a66f01d875e
SpectrumBlack <vsanlope@gmail.com>
2025-10-31 21:33:28 -0700

    J

---
+++ catrina-games/Scrips/Enemigo/chaman.gd
@@ -0,0 +1,86 @@
+extends CharacterBody2D
+
+var gravity = 15
+var walk_left = true
+var speed = 32
+var Stop = false
+var Dead = false
+
+@onready var anima = $Chaman
+@onready var Spri = $Chama
+@onready var Coll = $"Collision Cha"
+@onready var timer: Timer =$Timer 
+
+func _physics_process(_delta):
+	if Dead == true: 
+		return
+
+	if Vida.SeñalC == true:
+		anima.play("Hurt")
+		var timer : Timer = Timer.new()
+		add_child(timer)
+		timer.one_shot = true
+		timer.autostart = false
+		timer.wait_time = 1.5
+		timer.timeout.connect(func(): Vida.SeñalC = false)
+		timer.start()
+	else:
+		if Vida.Chaman <= 0:
+				anima.play("Death")
+				Dead = true
+		else:
+			Atacar()
+			Daño()
+			Caminar()
+			Turn() 
+	
+	
+func Atacar():
+	Stop = false
+	
+	if $"Izquierda".is_colliding():
+			Stop = true
+			anima.play("Atacar")
+			$"Hit".scale.x = 1
+			Spri.flip_h = false
+	else:
+		if $"Derecha".is_colliding():
+			Stop = true
+			$"Hit".scale.x = -1
+			anima.play("Atacar")
+			Spri.flip_h = true
+#Daño a jugador
+func Daño():
+	if $"Hit".is_colliding():
+		Vida.Live -= 1
+
+func Caminar():
+	if (Stop == false):
+		if (walk_left):
+			velocity.x = speed
+			anima.play("Walk")
+			
+
+		else:
+			velocity.x = -speed
+			anima.play("Walk")
+			Spri.flip_h =  false
+			
+		velocity.y += gravity
+		move_and_slide()
+	else:
+		velocity.x = 0 
+	move_and_slide()
+func Turn():
+	if not $Camino.is_colliding():
+		walk_left = !walk_left
+		scale.x = -scale.x
+func _on_AnimatedSprite_frame_change():
+	if anima.animation == "Death":
+		Dead = true
+func _on_AnimatedSprite_animation_finished():
+	if anima.animation == "Death":
+		Spri.visible = false
+
+
+	
